e23201efec574406b40cc039b50ae1bdc53bc796
Anett <anett.huebner@starwit.de>
2024-08-01 15:58:46 +0200

    test

--- persistence/src/main/java/de/starwit/persistence/PersistenceApplication.java
+++ persistence/src/main/java/de/starwit/persistence/PersistenceApplication.java
@@ -1,7 +1,9 @@
 package de.starwit.persistence;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @SpringBootApplication
 public class PersistenceApplication {
 
