6d444d8dfe29be9c43ad1ea528c68957990eda20
Anett <anett.huebner@starwit.de>
2024-08-01 16:02:41 +0200

    test

--- persistence/src/test/java/de/starwit/persistence/repository/MitigationActionRepositoryTest.java
+++ persistence/src/test/java/de/starwit/persistence/repository/MitigationActionRepositoryTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import de.starwit.persistence.entity.MitigationActionEntity;
@@ -14,8 +15,7 @@ import de.starwit.persistence.entity.MitigationActionEntity;
 /**
  * Tests for MitigationActionRepository
  */
-@DataJpaTest
-@ContextConfiguration
+@SpringBootTest(classes = { de.starwit.persistence.PersistenceApplication.class })
 public class MitigationActionRepositoryTest {
 
     @Autowired
