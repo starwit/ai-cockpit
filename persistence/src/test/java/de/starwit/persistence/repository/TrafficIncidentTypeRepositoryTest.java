e697413f24dbd7844fcff4b7a0d8951e8a7c43ef
Anett <anett.huebner@starwit.de>
2024-08-01 16:02:59 +0200

    juni test prerelease

--- persistence/src/test/java/de/starwit/persistence/repository/TrafficIncidentTypeRepositoryTest.java
+++ persistence/src/test/java/de/starwit/persistence/repository/TrafficIncidentTypeRepositoryTest.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 import de.starwit.persistence.entity.TrafficIncidentTypeEntity;
 
