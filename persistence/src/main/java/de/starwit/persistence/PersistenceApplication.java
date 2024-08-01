4d719a7b9fc440e0ff9fa41e1a570026cde84e41
Anett <anett.huebner@starwit.de>
2024-08-01 10:20:39 +0200

    remove postConstruct

--- persistence/src/main/java/de/starwit/persistence/PersistenceApplication.java
+++ persistence/src/main/java/de/starwit/persistence/PersistenceApplication.java
@@ -2,14 +2,7 @@ package de.starwit.persistence;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class PersistenceApplication {
-
-    @PostConstruct
-    void test() {
-        System.out.println("+++++++ persistence init called");
-    }
 
 }
