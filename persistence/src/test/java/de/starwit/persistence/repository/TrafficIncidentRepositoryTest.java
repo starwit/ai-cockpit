180aee06874f549d862ad17cacf1fde448709d9e
Anett <anett.huebner@starwit.de>
2024-08-01 17:30:53 +0200

    use main for test classes

--- persistence/src/test/java/de/starwit/persistence/repository/TrafficIncidentRepositoryTest.java
+++ persistence/src/test/java/de/starwit/persistence/repository/TrafficIncidentRepositoryTest.java
@@ -1,6 +1,6 @@
 package de.starwit.persistence.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
