f6ac8f16b43358a9a4b2cf0926009eb5e4aaf10f
Anett Hübner <anett_huebner@gmx.de>
2024-10-31 22:40:11 +0100

    corrected Database test

--- service/src/test/java/de/starwit/service/TrafficIncidentServiceTest.java
+++ service/src/test/java/de/starwit/service/TrafficIncidentServiceTest.java
@@ -1,46 +1,30 @@
 package de.starwit.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.testcontainers.shaded.com.google.common.base.Verify;
 
-import de.starwit.persistence.entity.MitigationActionEntity;
 import de.starwit.persistence.entity.TrafficIncidentEntity;
 import de.starwit.persistence.entity.TrafficIncidentTypeEntity;
-import de.starwit.persistence.repository.TrafficIncidentRepository;
-import de.starwit.persistence.repository.TrafficIncidentTypeRepository;
 import de.starwit.service.impl.TrafficIncidentService;
+import de.starwit.service.impl.TrafficIncidentTypeService;
 import de.starwit.visionapi.Reporting.IncidentMessage;
-
 
 @Import(TestServiceConfiguration.class)
 @SpringBootTest
-// @AutoConfigureTestDatabase 
-@PropertySource(value = "classpath:application.properties")
 public class TrafficIncidentServiceTest {
 
     @Autowired
-    private TrafficIncidentRepository trafficIncidentRepository;
-
-    @Autowired
-    private TrafficIncidentTypeRepository trafficIncidentTypeRepository;
-
-    @Autowired
     private TrafficIncidentService trafficIncidentService;
+
+    @Autowired
+    private TrafficIncidentTypeService trafficIncidentTypeService;
 
     @Test
     void testCreateNewIncidentWithMitigationActionsMessage() {
@@ -75,7 +59,22 @@ public class TrafficIncidentServiceTest {
 
     @Test
     void testFindByName() {
+
+        // prepare
+        IncidentMessage incidentMessage = mock(IncidentMessage.class);
+        when(incidentMessage.getMediaUrl()).thenReturn("http://testurl.com/media");
+        when(incidentMessage.getTimestampUtcMs()).thenReturn(1633046400000L);
+
+        TrafficIncidentTypeEntity defaultIncidentType = new TrafficIncidentTypeEntity();
+        defaultIncidentType.setName("dangerous driving behaviour");
+        trafficIncidentTypeService.saveOrUpdate(defaultIncidentType);
+
+        // Call Methode
+        TrafficIncidentEntity result = trafficIncidentService
+                .createNewIncidentWithMitigationActionsMessage(incidentMessage);
+
         TrafficIncidentTypeEntity entity = trafficIncidentService.findIncidentTypeByName("dangerous driving behaviour");
         assertTrue(entity.getName().equals("dangerous driving behaviour"));
+
     }
 }
