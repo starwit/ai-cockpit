fbc391ff16f804b72a6cfcc02b7ad644446b656b
Anett Hübner <anett_huebner@gmx.de>
2024-11-19 22:03:58 +0100

    corrected transaction behavior for tests

--- service/src/test/java/de/starwit/service/TrafficIncidentServiceTest.java
+++ service/src/test/java/de/starwit/service/TrafficIncidentServiceTest.java
@@ -9,10 +9,13 @@ import java.time.ZonedDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.starwit.persistence.entity.ExecutionPolicies;
 import de.starwit.persistence.entity.MitigationActionEntity;
@@ -21,20 +24,17 @@ import de.starwit.persistence.entity.TrafficIncidentEntity;
 import de.starwit.persistence.entity.TrafficIncidentTypeEntity;
 import de.starwit.persistence.repository.MitigationActionTypeRepository;
 import de.starwit.persistence.repository.TrafficIncidentTypeRepository;
-import de.starwit.service.impl.MitigationActionTypeService;
 import de.starwit.service.impl.TrafficIncidentService;
 import de.starwit.service.impl.TrafficIncidentTypeService;
 import de.starwit.visionapi.Reporting.IncidentMessage;
 
 @Import(TestServiceConfiguration.class)
+@Transactional
 @SpringBootTest
 public class TrafficIncidentServiceTest {
 
     @Autowired
     TrafficIncidentTypeRepository trafficIncidentTypeRepository;
-
-    @Autowired
-    private MitigationActionTypeService mitigationActionTypeService;
 
     @Autowired
     MitigationActionTypeRepository mitigationActionTypeRepository;
@@ -45,7 +45,28 @@ public class TrafficIncidentServiceTest {
     @Autowired
     private TrafficIncidentTypeService trafficIncidentTypeService;
 
-    //@Test
+    @Test
+    @Commit
+    @Order(1)
+    void testCreateTypes() {
+        TrafficIncidentTypeEntity defaultIncidentType = new TrafficIncidentTypeEntity();
+        defaultIncidentType.setName("dangerous driving behaviour");
+        MitigationActionTypeEntity actionType = new MitigationActionTypeEntity();
+        actionType.setDescription("Notify public platforms or apps about the traffic incident");
+        actionType.setName("notify public platform");
+        actionType.setExecutionPolicy(ExecutionPolicies.AUTOMATIC);
+        actionType = mitigationActionTypeRepository.save(actionType);
+
+        Set<MitigationActionTypeEntity> mitigationActionTypes = new HashSet<>();
+        defaultIncidentType.setMitigationActionType(mitigationActionTypes);
+        mitigationActionTypes.add(actionType);
+        defaultIncidentType = trafficIncidentTypeService.saveOrUpdate(defaultIncidentType);
+
+    }
+
+    @Test
+    @Commit
+    @Order(2)
     void testCreateNewIncidentWithMitigationActionsMessage() {
 
         // prepare
@@ -54,21 +75,6 @@ public class TrafficIncidentServiceTest {
         when(incidentMessage.getMediaUrl()).thenReturn("http://testurl.com/media");
         when(incidentMessage.getTimestampUtcMs()).thenReturn(timestamp);
 
-        TrafficIncidentTypeEntity defaultIncidentType = new TrafficIncidentTypeEntity();
-        defaultIncidentType.setName("dangerous driving behaviour");
-        MitigationActionTypeEntity actionType = new MitigationActionTypeEntity();
-        actionType.setDescription("Notify public platforms or apps about the traffic incident");
-        actionType.setName("notify public platform");
-        actionType.setExecutionPolicy(ExecutionPolicies.AUTOMATIC);
-        Set<MitigationActionTypeEntity> mitigationActionType = new HashSet<>();
-        defaultIncidentType.setMitigationActionType(mitigationActionType);
-        actionType = mitigationActionTypeService.saveOrUpdate(actionType);
-        mitigationActionType.add(actionType);
-        defaultIncidentType = trafficIncidentTypeService.saveOrUpdate(defaultIncidentType);
-
-        mitigationActionTypeRepository
-                    .findByTrafficIncidentType(defaultIncidentType.getId());
-
         // Call Methode
         TrafficIncidentEntity result = trafficIncidentService
                 .createNewIncidentWithMitigationActionsMessage(incidentMessage);
@@ -76,24 +82,25 @@ public class TrafficIncidentServiceTest {
         // Assert
         assertEquals("http://testurl.com/media", result.getMediaUrl());
         ZonedDateTime expectedDateTime = Instant.ofEpochMilli(timestamp)
-        .atZone(ZoneId.systemDefault());
+                .atZone(ZoneId.systemDefault());
+        assertTrue(expectedDateTime.isEqual(result.getAcquisitionTime()));
         assertEquals(expectedDateTime, result.getAcquisitionTime());
         assertEquals(1, result.getMitigationAction().size());
         MitigationActionEntity action = result.getMitigationAction().iterator().next();
-        assertEquals(expectedDateTime, action.getCreationTime());
+        assertTrue(expectedDateTime.isEqual(action.getCreationTime()));
     }
 
     @Test
     void testFindByName() {
-        //prepare
+        // prepare
         TrafficIncidentTypeEntity defaultIncidentType = new TrafficIncidentTypeEntity();
         defaultIncidentType.setName("dangerous driving behaviour");
         trafficIncidentTypeService.saveOrUpdate(defaultIncidentType);
 
-        //Call-Methode
+        // Call-Methode
         TrafficIncidentTypeEntity entity = trafficIncidentService.findIncidentTypeByName("dangerous driving behaviour");
 
-        //Assert
+        // Assert
         assertTrue(entity.getName().equals("dangerous driving behaviour"));
 
     }
