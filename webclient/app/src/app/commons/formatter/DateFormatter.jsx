9389c1c97a880f8a2dbd660b24be04e578d6ed1c
Anett Hübner <anett_huebner@gmx.de>
2024-12-06 14:15:44 +0100

    use short language date representation

--- webclient/app/src/app/commons/formatter/DateFormatter.jsx
+++ webclient/app/src/app/commons/formatter/DateFormatter.jsx
@@ -15,19 +15,16 @@ const formatDateFull = (isoString, i18n) => {
 
 const formatDateShort = (isoString, i18n) => {
     const date = new Date(isoString);
+    const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
 
-    const day = String(date.getUTCDate()).padStart(2, '0');
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const year = date.getUTCFullYear();
-
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-
-    if (i18n.language == "en-US") {
-        return `${month}.${day}.${year} ${hours}:${minutes}:${seconds}`;
-    }
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+    return date.toLocaleDateString(i18n.language, options);
 };
 
 export {
