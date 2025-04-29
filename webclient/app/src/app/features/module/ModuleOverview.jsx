f860709a3b2f09668ac0671066a3396cf9575528
Anett Hübner <anett_huebner@gmx.de>
2025-04-29 21:41:22 +0200

    added application view

--- webclient/app/src/app/features/module/ModuleOverview.jsx
+++ webclient/app/src/app/features/module/ModuleOverview.jsx
@@ -1,5 +1,5 @@
 import Lan from "@mui/icons-material/Lan";
-import {Container, Stack, Typography} from '@mui/material';
+import {Box, Card, CardContent, CardHeader, Container, Stack, Tab, Tabs, Typography} from '@mui/material';
 import Grid from "@mui/material/Grid";
 import React, {useEffect, useMemo, useState} from "react";
 import {useTranslation} from "react-i18next";
@@ -10,30 +10,101 @@ function ModuleOverview() {
     const {t} = useTranslation();
     const moduleFunctions = useMemo(() => new ModuleRest(), []);
     const [moduleData, setModuleData] = useState([]);
+    const [groupedModules, setGroupedModules] = useState([]);
+    const [applications, setApplications] = useState([]);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedName, setSelectedName] = useState();
 
     useEffect(() => {
         moduleFunctions.getWithDecisions().then((response) => {
             if (response.data == null) {
                 return;
             } else {
-                setModuleData(response.data);
+                reload(response.data);
+
             }
         });
     }, []);
 
+    useEffect(() => {
+        if (applications) {
+            setSelectedName(applications[selectedIndex]);
+        }
+    }, [selectedIndex, applications])
+
+    function reload(data) {
+        setModuleData(data);
+        let grouped = groupByApplication(data);
+        setGroupedModules(grouped);
+        setApplications(Object.keys(grouped));
+    }
+
+    function handleChange(event, newValue) {
+        setSelectedIndex(newValue);
+        setSelectedName(applications[newValue]);
+    };
+
+    // Group users by name
+    function groupByApplication(moduleData) {
+        const map = {};
+        moduleData.forEach(module => {
+            if (!map[module.applicationIdentifier]) {
+                map[module.applicationIdentifier] = [];
+            }
+            map[module.applicationIdentifier].push(module);
+        });
+        return map;
+    };
+
+    function renderApplicationsName() {
+        if (applications) {
+            return (
+                applications.map((name, index) => (
+                    <Tab label={name} key={index} />
+                ))
+            );
+        }
+    }
+
+    function renderGroupedModules() {
+        if (groupedModules[selectedName]) {
+            return (
+                groupedModules[selectedName].map(row => (
+                    <Grid key={row.id} size={{xs: 4, md: 4}}>
+                        <ModuleInfo module={row} />
+                    </Grid>
+                ))
+
+            );
+        }
+    }
+
     return (
         <Container sx={{paddingTop: 2}}>
-            <Typography variant="h2" sx={{paddingBottom: 0, marginBottom: 0}}>
-                <Lan fontSize="small" /> {t("module.heading")}
-            </Typography>
-            <Stack direction="row" sx={{marginTop: 1}}>
-                <Grid container spacing={2}>
-                    {moduleData.map(row => (
-                        <Grid size={{xs: 12, sm: 4}} key={row.id}>
-                            <ModuleInfo module={row} />
-                        </Grid>
-                    ))}
-                </Grid >
+
+            <Stack direction="row">
+                <Tabs
+                    value={selectedIndex}
+                    onChange={handleChange}
+                    orientation="vertical"
+                    variant="scrollable"
+                >
+                    {renderApplicationsName()}
+                </Tabs>
+
+                <Container>
+                    <Stack direction="row">
+                        <Typography variant="h2" sx={{paddingBottom: 0, marginBottom: 0}}>
+                            <Lan fontSize="small" /> {t("module.heading")}
+                        </Typography>
+                        <Typography variant="h2" sx={{paddingBottom: 0, marginBottom: 0}}>
+                            | {selectedName}
+                        </Typography>
+                    </Stack>
+                    <Grid container spacing={2} sx={{paddingTop: 2}}>
+                        {renderGroupedModules()}
+                    </Grid >
+                </Container>
             </Stack>
         </Container>
     )
